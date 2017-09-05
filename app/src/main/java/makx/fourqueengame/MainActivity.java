406768ebca43976353cefdda50a3c02cc5e48b4b
Mayank kumar <kr.mayank008@gmail.com>
2017-09-05 22:59:39 +0530

    Added three different modes. only 5x5 working as of now.

--- app/src/main/java/makx/fourqueengame/MainActivity.java
+++ app/src/main/java/makx/fourqueengame/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     String txt;
 
-    Button submitButton, clearButton;
+    Button submitButton, clearButton, button4x4, button5x5, button6x6;
 
     int stack[] = new int[25];
     int sTop = -1;
@@ -29,281 +29,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
 
+        button4x4 = (Button) findViewById(R.id.button4x4);
 
+        button4x4.setOnClickListener(this);
 
-        for(int i = 0; i < 5; i++)
-            for(int j = 0; j < 5; j++)
-            {
-                btn[i][j]= (Button) findViewById(buttonId[i][j]);
-                btn[i][j].setOnClickListener(this);
-            }
+        button5x5 = (Button) findViewById(R.id.button5x5);
 
-        submitButton = (Button) findViewById(R.id.submitbutton);
+        button5x5.setOnClickListener(this);
 
-        clearButton = (Button) findViewById(R.id.clearButton);
+        button6x6 = (Button) findViewById(R.id.button6x6);
 
-        clearButton.setOnClickListener(this);
-
-        submitButton.setOnClickListener(this);
+        button6x6.setOnClickListener(this);
 
     }
+
+    /*@Override
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_menu);
+
+        button4x4 = (Button) findViewById(R.id.button4x4);
+
+        button4x4.setOnClickListener(this);
+
+        button5x5 = (Button) findViewById(R.id.button5x5);
+
+        button5x5.setOnClickListener(this);
+
+        button6x6 = (Button) findViewById(R.id.button6x6);
+
+        button6x6.setOnClickListener(this);
+    }*/
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-
-            /*case (R.id.button00):
-                txt = btn[0][0].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[0][0].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[0][0].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button01):
-                txt = btn[0][1].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[0][1].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[0][1].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button02):
-                txt = btn[0][2].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[0][2].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[0][2].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button03):
-                txt = btn[0][3].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[0][3].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[0][3].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button04):
-                txt = btn[0][4].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[0][4].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[0][4].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button10):
-                txt = btn[1][0].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[1][0].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[1][0].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button11):
-                txt = btn[1][1].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[1][1].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[1][1].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button12):
-                txt = btn[1][2].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[1][2].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[1][2].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button13):
-                txt = btn[1][3].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[1][3].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[1][3].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button14):
-                txt = btn[1][4].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[1][4].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[1][4].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button20):
-                txt = btn[2][0].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[2][0].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[2][0].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button21):
-                txt = btn[2][1].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[2][1].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[2][1].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button22):
-                txt = btn[2][2].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[2][2].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[2][2].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button23):
-                txt = btn[2][3].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[2][3].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[2][3].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button24):
-                txt = btn[2][4].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[2][4].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[2][4].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button30):
-                txt = btn[3][0].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[3][0].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[3][0].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button31):
-                txt = btn[3][1].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[3][1].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[3][1].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button32):
-                txt = btn[3][2].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[3][2].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[3][2].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button33):
-                txt = btn[3][3].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[3][3].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[3][3].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button34):
-                txt = btn[3][4].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[3][4].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[3][4].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button40):
-                txt = btn[4][0].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[4][0].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[4][0].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button41):
-                txt = btn[4][1].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[4][1].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[4][1].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button42):
-                txt = btn[4][2].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[4][2].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[4][2].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button43):
-                txt = btn[4][3].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[4][3].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[4][3].setText(getString(R.string.space));
-                }
-                break;
-
-            case (R.id.button44):
-                txt = btn[4][4].getText().toString();
-                if(txt.equals(getString(R.string.space))){
-                    btn[4][4].setText(getString(R.string.queenShape));
-                }
-                else{
-                    btn[4][4].setText(getString(R.string.space));
-                }
-                break;*/
-
             case (R.id.submitbutton):
                 result = checkResult();
                 if (result == -1) {
@@ -321,6 +83,33 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         btn[i][j].setText(getString(R.string.space));
                     }
                 sTop = -1;
+                break;
+
+            case (R.id.button4x4):
+                Toast.makeText(this, "Not working yet, try 5x5", Toast.LENGTH_SHORT).show();
+                break;
+
+            case (R.id.button5x5):
+                setContentView(R.layout.activity_main);
+
+                for(int i = 0; i < 5; i++)
+                    for(int j = 0; j < 5; j++)
+                    {
+                        btn[i][j]= (Button) findViewById(buttonId[i][j]);
+                        btn[i][j].setOnClickListener(this);
+                    }
+
+                submitButton = (Button) findViewById(R.id.submitbutton);
+
+                clearButton = (Button) findViewById(R.id.clearButton);
+
+                clearButton.setOnClickListener(this);
+
+                submitButton.setOnClickListener(this);
+                break;
+
+            case (R.id.button6x6):
+                Toast.makeText(this, "Not working yet, try 5x5", Toast.LENGTH_SHORT).show();
                 break;
 
             default:
